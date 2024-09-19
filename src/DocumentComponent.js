375dda0206ab22af427e8094e93e6d1e9250276d
Muhammadamin Mamadaliyev <91719420+mamadaliyev66@users.noreply.github.com>
2024-09-19 21:34:17 +0500

    Update DocumentComponent.js

--- src/DocumentComponent.js
+++ src/DocumentComponent.js
@@ -65,7 +65,7 @@ export default function DocumentComponent() {
             );
           })
         ) : (
-          <div className="text-center">
+          <div className="text-center pt-['40%']">
           <div
     className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
     role="status">
