96a545c3ed071c93a8a9465ab18f79c52167fcca
Muhammadamin Mamadaliyev <91719420+mamadaliyev66@users.noreply.github.com>
2024-09-19 21:32:45 +0500

    Update DocumentComponent.js

--- src/DocumentComponent.js
+++ src/DocumentComponent.js
@@ -65,7 +65,16 @@ export default function DocumentComponent() {
             );
           })
         ) : (
-          <div className="text-center">404 Not Found!</div>
+          <div className="text-center">
+          <div
+    className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+    role="status">
+    <span
+      className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+    >Loading...</span>
+  </div>
+          
+          </div>
         )}
       </div>
     </div>
